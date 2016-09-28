737ef444d8b7aebbd907827208ff90802aa8f2b4
Vyacheslav Mayorov <vyacheslav.mayorov@gmail.com>
2016-09-28 05:05:36 +0300

    version comparator with multiple level of token comparison

--- src/main/java/com/github/acc15/version/VersionComparator.java
+++ src/main/java/com/github/acc15/version/VersionComparator.java
@@ -2,31 +2,55 @@ package com.github.acc15.version;
 
 import java.util.Comparator;
 
+import static com.github.acc15.version.Splitters.*;
+import static com.github.acc15.version.Tokens.*;
+
 public class VersionComparator implements Comparator<String> {
 
-    private final Splitter[] splitters;
-    private final Comparator<String> tokenComparator;
+    private final Level[] levels;
 
-    public VersionComparator(Comparator<String> tokenComparator, Splitter... splitters) {
-        this.splitters = splitters;
-        this.tokenComparator = tokenComparator;
+    public static Level splitBy(Splitter splitter) {
+        return new Level(splitter);
+    }
+
+    public static class Level {
+        private Splitter splitter;
+        private Comparator<String> tokenComparator;
+
+        public Level(Splitter splitter) {
+            this.splitter = splitter;
+            this.tokenComparator = Comparator.naturalOrder();
+        }
+
+        public Level orderBy(Tokens.TokenMatcher... matchers) {
+            this.tokenComparator = Tokens.toComparator(matchers);
+            return this;
+        }
+    }
+
+    public VersionComparator(Level... levels) {
+        this.levels = levels;
     }
 
     private static String emptyIfOutOfBounds(String[] arr, int idx) {
         return idx < arr.length ? arr[idx] : "";
     }
 
-    private int compare(String o1, String o2, int level) {
-        if (level >= splitters.length) {
+    private int compare(String o1, String o2, int level, Comparator<String> tokenComparator) {
+        if (level >= levels.length) {
             return tokenComparator.compare(o1, o2);
         }
 
-        Splitter splitter = splitters[level];
+        Splitter splitter = levels[level].splitter;
         String[] t1 = splitter.split(o1), t2 = splitter.split(o2);
 
         int l = Math.max(t1.length, t2.length);
+        if (l > 1 && !o1.isEmpty() && !o2.isEmpty()) {
+            tokenComparator = levels[level].tokenComparator;
+        }
+
         for (int i = 0; i < l; i++) {
-            int cmp = compare(emptyIfOutOfBounds(t1, i), emptyIfOutOfBounds(t2, i), level + 1);
+            int cmp = compare(emptyIfOutOfBounds(t1, i), emptyIfOutOfBounds(t2, i), level + 1, tokenComparator);
             if (cmp != 0) {
                 return cmp;
             }
@@ -34,24 +58,17 @@ public class VersionComparator implements Comparator<String> {
         return 0;
     }
 
+    public static VersionComparator createDefault() {
+        return new VersionComparator(
+            splitBy(pattern("\\.")).orderBy(other(), numeric(), empty()),
+            splitBy(pattern("\\-")).orderBy(other(), empty(), numeric()),
+            splitBy(digits()).orderBy(empty(), other(), numeric())
+        );
+    }
+
     @Override
     public int compare(String o1, String o2) {
-        return compare(o1, o2, 0);
-    }
-
-    public static class Builder {
-        private Splitter[] splitters;
-        public Builder(Splitter[] splitters) {
-            this.splitters = splitters;
-        }
-
-        public VersionComparator orderBy(Tokens.TokenMatcher... matchers) {
-            return new VersionComparator(Tokens.toComparator(matchers), splitters);
-        }
-    }
-
-    public static Builder splitBy(Splitter... others) {
-        return new Builder(others);
+        return compare(o1, o2, 0, levels[0].tokenComparator);
     }
 
 }
--- src/test/java/com/github/acc15/version/VersionComparatorTest.java
+++ src/test/java/com/github/acc15/version/VersionComparatorTest.java
@@ -12,27 +12,47 @@ public class VersionComparatorTest {
     @Test
     public void testCompare() throws Exception {
 
-        Comparator<String> cmp = VersionComparator.splitBy(
-            Splitters.pattern("\\."),
-            Splitters.pattern("\\-")
-        ).orderBy(
-            Tokens.other(),
-            Tokens.numeric(),
-            Tokens.empty()
-        );
+        Comparator<String> cmp = VersionComparator.createDefault();
 
         List<String> versions = Arrays.asList(
             "5.0.43.21",
             "5.0.43.21-SNAPSHOT",
-            "5.0.32.34");
+            "5.0.32.34",
+            "5.0.32-alpha.34",
+            "6.42b7-rc",
+            "5.0.32-beta.34",
+            "5.0.32-rc.34",
+            "5.0.32-1.34",
+            "6.42",
+            "6.42b10",
+            "6.42b7",
+            "6.42b",
+            "1.00-rc2",
+            "1.00",
+            "1.00-rc1",
+            "6.42b7-rc2",
+            "1.00-rc");
 
         versions.sort(cmp);
         versions.forEach(System.out::println);
-
         assertEquals(versions, Arrays.asList(
+            "1.00-rc",
+            "1.00-rc1",
+            "1.00-rc2",
+            "1.00",
+            "5.0.32-alpha.34",
+            "5.0.32-beta.34",
+            "5.0.32-rc.34",
             "5.0.32.34",
+            "5.0.32-1.34",
             "5.0.43.21-SNAPSHOT",
-            "5.0.43.21"
+            "5.0.43.21",
+            "6.42",
+            "6.42b",
+            "6.42b7-rc",
+            "6.42b7-rc2",
+            "6.42b7",
+            "6.42b10"
         ));
 
     }
